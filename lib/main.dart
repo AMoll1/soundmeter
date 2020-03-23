a3f1bf1b3fc8e74b3b53999e0e4dc8de566ddae5
T-Otti <thomas.otti@gmx.net>
2020-03-23 19:20:13 +0100

    Test comment

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application. asfdsdaf
+  // Thomas test 1
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
